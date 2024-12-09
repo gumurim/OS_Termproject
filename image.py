608c16ea1a124c2466787f6d465b1c24f657c979
gumurim <shinyl0727@naver.com>
2024-12-09 20:15:54 +0900

    Update image.py

--- image.py
+++ image.py
@@ -1,23 +1,19 @@
 import torch
-import torchvision.transforms as T
+import torchvision.transforms as transforms
 from PIL import Image
 import matplotlib.pyplot as plt
 
-# 이미지 경로 설정 및 로드
+# 이미지 로드
 image_path = "capture.JPG"
-image = Image.open(image_path).convert('RGB')
+image = Image.open(image_path)
 
 # 모델 로드
 model = torch.hub.load('pytorch/vision:v0.10.0', 'deeplabv3_resnet101', pretrained=True)
-model.eval()
 
 # 이미지 전처리
-preprocess = T.Compose([
-    T.ToTensor(),
-    T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-])
-input_tensor = preprocess(image).unsqueeze(0)
+transform = transforms.ToTensor()
+image_tensor = transform(image)
 
-# 결과 시각화
+# 결과 확인
 plt.imshow(image)
 plt.show()
